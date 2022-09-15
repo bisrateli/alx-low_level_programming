ecd6aebf3d5da797a849aab392d6014f33ae9994
bisrateli <bisrateli4@gmail.com>
2022-09-15 11:45:29 -0700

    isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file for prototypes
- * void prototype (void)
- * int prototype (int)
- */
+#include <stdio.h>
 
 int _putchar(char);
 int _isupper(int c);
